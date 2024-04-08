d6a609e4871ae4d499103089ed64b0f70d154e51
erick <erick.krug@skysoftware.com.br>
2024-04-08 15:32:04 -0300

    - Back / Front comunicando - Back agora lé arquivos Xlsx

--- MVCCampanha/Controllers/ReadXlsx.cs
+++ MVCCampanha/Controllers/ReadXlsx.cs
@@ -1,6 +1,55 @@
-﻿namespace MVCCampanha.Controllers
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ReadXlsxController : ControllerBase
 {
-    public class ReadXlsx
+    [HttpPost]
+    public List<string> Importar(string path)
     {
+        List<string> ids = new List<string>();
+        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        var file = Request.Form.Files["arquivo"];
+        var stream = file.OpenReadStream();
+        try
+        {
+
+            using (ExcelPackage excelPackage = new(stream))
+            {
+                var worksheet = excelPackage.Workbook.Worksheets.First();
+
+                int totalRows = worksheet.Dimension.End.Row;
+
+               
+                for (int rowNum = 1; rowNum <= totalRows; rowNum++)
+                {
+                    var cell = worksheet.Cells[rowNum, 1]; 
+
+                    if (cell.Value != null) 
+                    {
+                        string ?cellValue = cell.Value.ToString();
+
+                        if (!string.IsNullOrEmpty(cellValue.Trim()))
+                        {
+                            ids.Add(cellValue); 
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Erro ao extrair IDs do arquivo Excel: {ex.Message}");
+           
+        }
+
+        return ids;
     }
+
+
 }
+
